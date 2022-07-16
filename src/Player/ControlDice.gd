a194db3a3bc0892a534bbeb3dfe10afeebd0f4fa
JayAiJoe <jayailocsin@gmail.com>
2022-07-16 11:58:33 +0800

    random dice face

--- src/Player/ControlDice.gd
+++ src/Player/ControlDice.gd
@@ -3,9 +3,13 @@ extends Node2D
 class_name ControlDice
 
 
-func roll(direction : int):
+func roll(direction : int) -> void:
 	$MoveTween.interpolate_property(self, "global_position", global_position, global_position + POS.directions[direction] * POS.tile_size, 0.12, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	$MoveTween.start()
 	yield($MoveTween,"tween_completed")
-
+	$Faces.frame = get_face_up() - 1
+	
+	
+func get_face_up() -> int : 
+	return randi() % 6 + 1
 
