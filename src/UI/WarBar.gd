25e786876d212fc093e5beba832b8328f2a62a90
JayAiJoe <jayailocsin@gmail.com>
2022-07-17 18:51:38 +0800

    ui update

--- src/UI/WarBar.gd
+++ src/UI/WarBar.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func set_value(value : float) -> void:
-	$Tween.interpolate_property($ProgressBar, "value", $ProgressBar.value, value, 0.7 , Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	$Tween.interpolate_property($TextureProgress, "value", $TextureProgress.value, value, 0.7 , Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	$Tween.interpolate_property($TextureRect, "anchor_left", $TextureRect.anchor_left, value, 0.7 , Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	$Tween.interpolate_property($TextureRect, "anchor_right", $TextureRect.anchor_right, value, 0.7 , Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	$Tween.start()
