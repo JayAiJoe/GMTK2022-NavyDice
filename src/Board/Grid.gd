98175face2f49f50c226a471612ee9d924657f9e
JayAiJoe <jayailocsin@gmail.com>
2022-07-17 10:56:48 +0800

    bug fix

--- src/Board/Grid.gd
+++ src/Board/Grid.gd
@@ -87,7 +87,7 @@ func move_dice(direction : int) -> void:
 				equipments[get_equip_id(destination)]["number"] += 1
 	elif destination.x == loading_edge:
 		yield(current_dice.slide(direction), "completed")
-		dice_position = destination
+		#dice_position = destination
 	moving = false
 
 func is_in_grid(coordinates : Vector2) -> bool:
@@ -106,7 +106,7 @@ func wrong_dice():
 	yield($PenaltyTimer, "timeout")
 	current_dice.wrong()
 	yield(current_dice.slide(undo_dir), "completed")
-	dice_position = destination
+	#dice_position = destination
 
 func get_tile_state(coordinates : Vector2) -> int:
 	return _grid[coordinates.y][coordinates.x]
